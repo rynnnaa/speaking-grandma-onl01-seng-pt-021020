42ac1541d8c39f75d9cb6c518f69c8df0b4f9bad
Ryna Truong <rynatruong@outlook.com>
2020-02-13 03:43:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+  if phrase !== phrase.downcase
+    return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
   else  
     return "HUH?! SPEAK UP, SONNY!"
   end
